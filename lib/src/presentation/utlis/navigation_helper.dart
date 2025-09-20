d27986ba7805522dc8e15fe953b4ea848d51d0bf
Gautam <gk24feb@gmail.com>
2025-09-21 04:35:32 +0530

    refactor: reorganize imports and enhance meal-related data models

--- lib/src/presentation/utlis/navigation_helper.dart
+++ lib/src/presentation/utlis/navigation_helper.dart
@@ -1,45 +1,51 @@
 // lib/src/presentation/utils/navigation_helper.dart
 import 'package:foodam/core/constants/app_route_constant.dart';
 import 'package:foodam/core/service/logger_service.dart';
-import 'package:foodam/src/domain/entities/meal_entity.dart';
 import 'package:foodam/src/domain/entities/susbcription_entity.dart';
+
+import '../../domain/entities/meal/meal_entity.dart';
 
 // Simulate navigation without actual implementation
 class NavigationHelper {
   final LoggerService _logger = LoggerService();
-  
+
   void navigateToHome() {
     _logger.i('Navigating to home screen', tag: 'NAVIGATION');
     _simulateNavigation(AppRoutes.home);
   }
-  
+
   void navigateToLogin() {
     _logger.i('Navigating to login screen', tag: 'NAVIGATION');
     _simulateNavigation(AppRoutes.login);
   }
-  
+
   void navigateToPlanSelection() {
     _logger.i('Navigating to plan selection screen', tag: 'NAVIGATION');
     _simulateNavigation(AppRoutes.planSelection);
   }
-  
- 
-  
+
   void navigateToMealCustomization(Meal meal, String mealType, DateTime date) {
-    _logger.i('Navigating to meal customization screen: ${meal.id}', tag: 'NAVIGATION');
-    _simulateNavigation(AppRoutes.mealCustomization, arguments: {
-      'meal': meal,
-      'mealType': mealType,
-      'date': date,
-    });
+    _logger.i(
+      'Navigating to meal customization screen: ${meal.id}',
+      tag: 'NAVIGATION',
+    );
+    _simulateNavigation(
+      AppRoutes.mealCustomization,
+      arguments: {'meal': meal, 'mealType': mealType, 'date': date},
+    );
   }
-  
- 
+
   void navigateToActivePlan(Subscription subscription) {
-    _logger.i('Navigating to active plan screen: ${subscription.id}', tag: 'NAVIGATION');
-    _simulateNavigation(AppRoutes.activePlan, arguments: {'subscription': subscription});
+    _logger.i(
+      'Navigating to active plan screen: ${subscription.id}',
+      tag: 'NAVIGATION',
+    );
+    _simulateNavigation(
+      AppRoutes.activePlan,
+      arguments: {'subscription': subscription},
+    );
   }
-  
+
   void _simulateNavigation(String route, {Map<String, dynamic>? arguments}) {
     _logger.d('Navigation simulation: $route', tag: 'NAVIGATION');
     if (arguments != null) {
--- meal_entity.dart
+++ meal_entity.dart
@@ -1,20 +1,21 @@
-// lib/src/domain/entities/meal_slot_entity.dart
-import 'package:equatable/equatable.dart';
-import 'package:foodam/src/domain/entities/meal_entity.dart';
-
-class MealSlot extends Equatable {
-  final String day;
-  final String timing;
-  final Meal? meal;
-  final String? mealId;
-
-  const MealSlot({
-    required this.day,
-    required this.timing,
-    this.meal,
-    this.mealId,
-  });
-
-  @override
-  List<Object?> get props => [day, timing, meal, mealId];
-}
+// // lib/src/domain/entities/meal_slot_entity.dart
+// import 'package:equatable/equatable.dart';
+// import 'package:foodam/src/domain/entities/meal/meal_entity.dart';
+// import 'package:foodam/src/domain/entities/meal_entity.dart';
+//
+// class MealSlot extends Equatable {
+//   final String day;
+//   final String timing;
+//   final Meal? meal;
+//   final String? mealId;
+//
+//   const MealSlot({
+//     required this.day,
+//     required this.timing,
+//     this.meal,
+//     this.mealId,
+//   });
+//
+//   @override
+//   List<Object?> get props => [day, timing, meal, mealId];
+// }
